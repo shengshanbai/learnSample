f69277355b4addc0f1adb15c0586a06dd422572f
shengshanbai <shengshanbai@126.com>
2018-05-11 17:28:53 +0800

    first commit
    Change-Id: I5c4061baccc33b0e1fc0742244e6435e6f6c6dd1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <caffe/caffe.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <algorithm>
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace caffe;
+using std::string;
+
+typedef std::pair<string,float> Prediction;
+
+int main(){
+	return 0;
+}
