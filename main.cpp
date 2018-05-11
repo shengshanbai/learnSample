6e74d4b72cb9e81764a3c9e3abc9213eec78a11e
shengshanbai <shengshanbai@126.com>
2018-05-11 18:07:29 +0800

    windows 最近版
    Change-Id: Ia09712676bc6100fe3c10c8c6d8535552f359c77

--- main.cpp
+++ main.cpp
@@ -14,6 +14,67 @@ using std::string;
 
 typedef std::pair<string,float> Prediction;
 
+class Classifier{
+public:
+    Classifier(const string& model_file,
+                 const string& trained_file,
+                 const string& mean_file,
+                 const string& label_file);
+    std::vector<Prediction> Classify(const cv::Mat& img,int N=5);
+private:
+    void SetMean(const string& mean_file);
+    std::vector<float> Predict(const cv::Mat& img);
+    void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+    void Preprocess(const cv::Mat& img,std::vector<cv::Mat>* input_channels);
+private:
+    shared_ptr<Net<float>> net_;
+    cv::Size input_geometry_;
+    int num_channels_;
+    cv::Mat mean_;
+    std::vector<string> labels_;
+};
+
+Classifier::Classifier(const string& model_file,
+                       const string& trained_file,
+                       const string& mean_file,
+                       const string& label_file) {
+    Caffe::set_mode(Caffe::CPU);
+    /* Load the network. */
+    net_.reset(new Net<float>(model_file, TEST));
+    net_->CopyTrainedLayersFrom(trained_file);
+    CHECK_EQ(net_->num_inputs(), 1) << "Network should have exactly one input.";
+    CHECK_EQ(net_->num_outputs(), 1) << "Network should have exactly one output.";
+    Blob<float>* input_layer = net_->input_blobs()[0];
+    num_channels_=input_layer->channels();
+    CHECK(num_channels_ == 3 || num_channels_ == 1)
+    << "Input layer should have 1 or 3 channels.";
+    input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
+    /* Load the binaryproto mean file. */
+    SetMean(mean_file);
+    /* Load labels. */
+    std::ifstream labels(label_file.c_str());
+    CHECK(labels) << "Unable to open labels file " << label_file;
+    string line;
+    while (std::getline(labels, line))
+        labels_.push_back(string(line));
+    Blob<float>* output_layer = net_->output_blobs()[0];
+    CHECK_EQ(labels_.size(), output_layer->channels())
+    << "Number of labels is different from the output layer dimension.";
+}
+
+static bool PairCompare(const std::pair<float, int>& lhs,
+                        const std::pair<float, int>& rhs) {
+  return lhs.first > rhs.first;
+}
+
+/* Return the indices of the top N values of vector v. */
+static std::vector<int> Argmax(const std::vector<float>& v,int N){
+    std::vector<std::pair<float,int>> pairs;
+    for(size_t i=0;i<v.size();++i)
+        pairs.push_back(std::make_pair(v[i],i));
+
+}
+
 int main(){
 	return 0;
 }
